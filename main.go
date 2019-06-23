47512cd39e0142a04d33665678f77c82f831f311
Sagar Choudhary <sagar.choudhary96@gmail.com>
2019-06-23 20:58:53 +0530

    Add: query field to fetch post by ID

--- main.go
+++ main.go
@@ -17,11 +17,57 @@ var queryType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "HackerNews",
 	Fields: graphql.Fields{
 		"latestPost": &graphql.Field{
-			Type: graphql.String,
+			Type: graphql.NewList(storyType),
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				fetchStories()
-				return "Hello World", nil
+
+				res, err := fetchStories()
+				if err != nil {
+					return nil, err
+				}
+				return res, nil
 			},
+			Description: "Fetch top 20 posts",
+		},
+		"post": &graphql.Field{
+			Type: storyType,
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type:        graphql.Int,
+					Description: "Search for post by ID",
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				id := p.Args["id"].(int)
+				res, err := fetchStoryByID(int64(id))
+				if err != nil {
+					return nil, err
+				}
+
+				return res, nil
+			},
+			Description: "Fetch Single Post",
+		},
+	},
+})
+
+// Define StoryType
+var storyType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Story",
+	Fields: graphql.Fields{
+		"By": &graphql.Field{
+			Type: graphql.String,
+		},
+		"ID": &graphql.Field{
+			Type: graphql.String,
+		},
+		"Title": &graphql.Field{
+			Type: graphql.String,
+		},
+		"Score": &graphql.Field{
+			Type: graphql.String,
+		},
+		"URL": &graphql.Field{
+			Type: graphql.String,
 		},
 	},
 })
@@ -32,18 +78,34 @@ var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
 
 // create story struct
 type Story struct {
-	by    string
-	id    int
-	score int
-	title string
-	url   string
+	By    string `json:"by"`
+	ID    int    `json:"id"`
+	Score int    `json:"score"`
+	Title string `json:"title"`
+	URL   string `json:"url"`
 }
 
-// func fetchStoryById(id string) Story {
+func fetchStoryByID(id int64) (Story, error) {
 
-// }
-func fetchStories() {
-	// make request to this
+	// create story url
+	storyURL := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%v.json?print=pretty", id)
+
+	// fetch user story
+	response, err := http.Get(storyURL)
+
+	if err != nil {
+		return Story{}, err
+	}
+
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	story := Story{}
+	json.Unmarshal(body, &story)
+	return story, nil
+
+}
+func fetchStories() ([]Story, error) {
+	// make request to get stories ids
 	response, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
 	if err != nil {
 		fmt.Println(err)
@@ -51,11 +113,21 @@ func fetchStories() {
 
 	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
-	stories := make([]int64, 0)
-	json.Unmarshal(body, &stories)
-	fmt.Println(stories)
+	storyIds := make([]int64, 0)
+	json.Unmarshal(body, &storyIds)
 
-	//for fist 20 make request to fetch stories
+	stories := make([]Story, 0)
+
+	for i := 0; i < 20; i++ {
+		story, err := fetchStoryByID(storyIds[i])
+
+		if err != nil {
+			return nil, err
+		}
+
+		stories = append(stories, story)
+	}
+	return stories, err
 }
 
 func main() {
